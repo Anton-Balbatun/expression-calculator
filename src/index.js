10f563ffc4873ff44672ef26843a0e3a066b9ab7
Anton Balbatun <anton553322@gmail.com>
2019-10-11 17:08:00 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ function eval() {
 
 function expressionCalculator(expr) {
     // write your solution here
+    let result = 0
+    try {
+        result = result + (new Function('return ' + expr ))();
+    } catch (err) {
+        throw new Error ('ExpressionError: Brackets must be paired.');
+    }
+    if (result === Infinity) throw new Error ('TypeError: Devision by zero.');
+    
+        return result
+    
 }
 
 module.exports = {
